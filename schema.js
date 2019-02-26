c0f0028089ce2b0552d67e3896af3b88e93c22d4
Gray Smith <gray.smith00@gmail.com>
2019-02-25 21:59:02 -0700

    Setup basic server and schema.js

---
+++ schema.js
@@ -0,0 +1,10 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+module.exports = new GraphQLSchema({});
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log(`Server running on PORT 4000`);
+});
